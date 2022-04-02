c7c6b3343c99e86e69afeba59beece505277863f
Michael <mklink@yandex.ru>
2022-04-02 19:19:16 +0300

    ЗАДАНИЕ 4.5.4 (HW-03)

---
+++ 16E4/04.js
@@ -0,0 +1,60 @@
+﻿function Device(name,power){
+  this.name = name;
+  this.isPlugged = false;
+  this.power = power;
+}
+
+//Вкл или Выкл прибор
+Device.prototype.plug = function(myAction){
+  this.isPlugged = myAction;
+}
+
+//Текущее потребление прибора
+Device.prototype.getCurrentPower = function(){
+  if (this.isPlugged)
+    return this.power;
+  else
+    return 0;
+}
+
+function DiveceLamp(cordLength){
+  this.cordLength=cordLength
+}
+
+DiveceLamp.prototype = new Device ('Lamp',40);
+
+
+function DiveceComputer(processorType){
+  this.processorType=processorType  
+}
+
+DiveceComputer.prototype = new Device ('Computer',400);
+
+DiveceComputer.prototype.getSpecification = function(){
+  return `${this.name} основан на платформе ${this.processorType}.`;
+}
+
+
+
+const lamp = new DiveceLamp('2м');
+const computer = new DiveceComputer('AMD');
+
+
+
+//Включаем лампу и смотрим потребление
+lamp.plug(true);
+
+console.log(lamp.getCurrentPower());
+console.log(computer.getCurrentPower());
+
+//Выключаем лампу смотрим потребление
+lamp.plug(false);
+console.log(lamp.name+' потребляет '+lamp.getCurrentPower());
+
+//Включаем компьютер смотрим спецификацию и потребление
+computer.plug(true);
+console.log(computer.getSpecification());
+console.log(computer.name+' потребляет '+computer.getCurrentPower());
+
+console.log(lamp);
+console.log(computer);
---
+++ 16E4/05.js
@@ -0,0 +1,71 @@
+﻿class Device{
+  constructor (name,power)
+     {
+       this.name = name;
+       this.isPlugged = false;
+       this.power = power;
+    }
+
+
+//Вкл или Выкл прибор
+  plug(myAction){
+     this.isPlugged = myAction;
+  }
+
+//Текущее потребление прибора
+   getCurrentPower (){
+     if (this.isPlugged)
+        return this.power;
+     else
+        return 0;
+   }
+
+}
+
+
+class DiveceLamp extends Device{
+  constructor (cordLength){
+    super('Lamp',40);
+    this.cordLength=cordLength;
+  }  
+}
+
+
+class DiveceComputer extends Device{
+  constructor (processorType){
+    super('Computer',400);
+    this.processorType=processorType;
+  }
+  
+ getSpecification(){
+    return `${this.name} основан на платформе ${this.processorType}.`;
+  }
+  
+}
+
+
+
+
+
+const lamp = new DiveceLamp('2м');
+const computer = new DiveceComputer('AMD');
+
+
+
+//Включаем лампу и смотрим потребление
+lamp.plug(true);
+
+console.log(lamp.getCurrentPower());
+console.log(computer.getCurrentPower());
+
+//Выключаем лампу смотрим потребление
+lamp.plug(false);
+console.log(lamp.name+' потребляет '+lamp.getCurrentPower());
+
+//Включаем компьютер смотрим спецификацию и потребление
+computer.plug(true);
+console.log(computer.getSpecification());
+console.log(computer.name+' потребляет '+computer.getCurrentPower());
+
+console.log(lamp);
+console.log(computer);
